87865bc216a77a7878049d28f63a3822cdae77a3
root <root@bally-laptop.localdomain>
2025-12-26 13:24:20 +0500

    main back logic added 2

--- lib/apiClient.ts
+++ lib/apiClient.ts
@@ -101,9 +101,9 @@ export const apiClient = async <T = any>(
   
   const token = await getAuthToken(url, options)
   
-  const headers: HeadersInit = {
+  const headers: Record<string, string> = {
     'Content-Type': 'application/json',
-    ...options.headers,
+    ...(options.headers as Record<string, string>),
   }
 
   if (token) {
